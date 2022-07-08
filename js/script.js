672415ab6aa189ba75dcb7471521f27361c122ef
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-07-07 20:48:20 -0500

    se agrego las llamadas de los datos a sus respectivos contenedores

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+let res = fetch("./data.json")
+    .then((res) => {
+        // console.log(res);
+        if (res.ok) {
+            return res.json();
+        }
+        else {
+            Promise.reject(res);
+        }
+    })
+    .then(json => {
+        //  console.log(json);
+        let gRegistro = document.querySelector(".registro");
+        let lblTiempo = document.querySelectorAll(".lbTiempo");
+        let lblLasTime = document.querySelectorAll(".last");
+        console.log(lblTiempo);
+        gRegistro.addEventListener("click", (e) => {
+            let registro = e.target.textContent;
+            let contador = 0;
+            // console.log(registro);
+            if (e.target.matches(".reg")) {//referencia a etiquetas Dayli Weekly Monthly
+                json.forEach(el => {
+                    for (const key in el.timeframes) {
+                        if (key === registro.toLowerCase()) {
+                            //  console.log(key);
+                            //console.log(el.timeframes[key].current);
+                            console.log(`${el.timeframes[key].current}hrs`);
+                            lblTiempo[contador].innerHTML = `${el.timeframes[key].current}hrs`;
+                            lblLasTime[contador].innerHTML = `last week - ${el.timeframes[key].previous}hrs`;
+                            contador++;
+                        };
+                    };
+                });
+            }
+            else {
+                console.log("se equivoco");
+            };
+        });
+    });
